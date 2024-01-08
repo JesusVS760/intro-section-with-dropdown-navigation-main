708b6124ed3f59ee7b6233f82bb9fb0bf4ec8d05
JesusVA760 <tonio4340406@gmail.com>
2024-01-07 22:44:54 -0800

    progress

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
-const displayOption = document.querySelectorAll(".text__option");
+//const displayOptions = document.querySelectorAll(".options");
+const expandableOptions = document.querySelectorAll(".expandable");
+const dropEffect = document.querySelectorAll(".dropDown");
 
 menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
 
-displayOptions.forEach((option) => {
-  option.addEventListener("click", () => {
-    option.classList.remove("expanded");
+expandableOptions.forEach((option) => {
+  option.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const arrowImg = this.nextElementSibling.querySelector(".arrow-img");
+
+    arrowImg.classList.toggle("rotate");
+  });
+});
+
+dropEffect.forEach((element) => {
+  element.addEventListener("click", function () {
+    const expandedElement = this.querySelector(".expanded__company");
+    expandedElement.classList.toggle("expanded");
   });
 });
